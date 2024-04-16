de9a772fc99caba74faa0b81eab65b7e33a62707
Jhayjay00 <davjiy@gmail.com>
2024-04-16 14:18:53 -0600

    to do commit

--- lib/screens/my_home_page.dart
+++ lib/screens/my_home_page.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:table_calendar/table_calendar.dart';
@@ -14,140 +12,157 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
   FirebaseFirestore db = FirebaseFirestore.instance;
 
   final List<String> tasks = <String>[];
 
   final List<bool> checkboxes = List.generate(8, (index) => false);
 
-  bool isChecked  = false;
+  bool isChecked = false;
+
+  /*
+  The TextEditingController class allows us to 
+  grab the input from the TextField() widget
+  This will be used later on to store the value
+  in the database.
+  */
 
   TextEditingController nameController = TextEditingController();
 
-    void addItemToList() async {
-      final String taskName = nameController.text;
+  void addItemToList() async {
+    final String taskName = nameController.text;
 
-      await db.collection('tasks').add({
-        'name': taskName,
-        'completed': isChecked,
-        'timestamp':FieldValue.serverTimestamp(),
-      });
+    //Add to the Firestore collection
+    await db.collection('tasks').add({
+      'name': taskName,
+      'completed': null,
+      'timestamp': FieldValue.serverTimestamp(),
+    });
 
+    setState(() {
+      tasks.insert(0, taskName);
+      checkboxes.insert(0, false);
+    });
+  }
 
-      setState(() {
-        tasks.insert(0, taskName);
-        checkboxes.insert(0, false);
-      });
+Future<void> fetchTaskFromFirestore() async {
 
-    }
+  //get a reference to the 'task' collection in Firestore
 
-   @override
+  CollectionReference taskCollection = db.collection('tasks');
+
+  //fetch the documents (tasks) from the collection
+  QuerySnapshot querySnapshot = await taskCollection.get();
+
+  //create an empty list to store the fetch tasks names
+  List<String> fetchedTasks = [];
+
+  //look through each doc (task) in the querySnapshot object
+
+  for (QueryDocumentSnapshot docSnapshot in querySnapshot.docs){
+    //Getting the task name from the documents's data
+    String taskName = docSnapshot.get('name');
+
+    //Getting the completion status ofthe task
+    bool completed = docSnapshot.get('completed');
+
+    //Add the task name to the list of the fetched data.
+    fetchedTasks.add(taskName);
+  } 
+}
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color.fromARGB(220, 21, 75, 221),
+        /*
+            Rows() and Columns() both have the mainAxisAlignment 
+            property we can utilize to space out their child 
+            widgets to our desired format.
+           */
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             SizedBox(
-              height: 80,
+              /*
+            SizedBox allows us to control the vertical 
+            and horizontal dimensions by manipulating the 
+            height or width property, or both.
+            */
+              height: 70,
               child: Image.asset('assets/stand.jpg'),
             ),
-            const Text('Standin on Bizness',
-                style: TextStyle(
-                  fontFamily: 'Caveat',
-                  fontSize: 32,
-                )),
+            Text(
+              'Standin on bizness',
+              style: TextStyle(
+                fontFamily: 'Caveat',
+                fontSize: 32,
+              ),
+            ),
           ],
         ),
       ),
       body: Container(
-        color: Colors.grey[200], // Change the background color
+        color: Color.fromARGB(255, 50, 116, 216),
         child: Column(
-          children: <Widget>[
-            Container(
-              height: 300,
-              color: Colors.white, // Change the background color
-              child: Center(
+          children: [
+            Expanded(
+              child: Container(
+                height: 300,
+                color: Color.fromARGB(255, 227, 232, 245),
                 child: Padding(
-                  padding: const EdgeInsets.only(top: 15.0),
-                  child: TableCalendar(
-                    calendarFormat: _calendarFormat,
+                  padding: const EdgeInsets.all(12.0),
+                  child:
+                      /*
+          The TableCalendar() widget below is installed via 
+          "flutter pub get table_calendar" or by adding the package 
+          to the pubspec.yaml file.  We then import it and implement using
+          configuration properties.  You can set a range and a focus day. 
+          The particulars of implementation for any package can be gleaned 
+          from pub.dev: https://pub.dev/packages/table_calendar.
+          */
+                      TableCalendar(
+                    calendarFormat: CalendarFormat.month,
                     headerVisible: false,
-                    focusedDay: _focusedDay,
-                    firstDay: DateTime(2022),
-                    lastDay: DateTime(2030),
-                    selectedDayPredicate: (day) {
-                      return isSameDay(_selectedDay, day);
-                    },
-                    onDaySelected: (selectedDay, focusedDay) {
-                      setState(() {
-                        _selectedDay = selectedDay;
-                        _focusedDay = focusedDay; // Update the focused day
-                      });
-                    },
+                    focusedDay: DateTime.now(),
+                    firstDay: DateTime(2023),
+                    lastDay: DateTime(2025),
                   ),
                 ),
               ),
             ),
-            Expanded(
-              child: ShaderMask(
-                shaderCallback: (Rect bounds) {
-                  return LinearGradient(
-                    begin: Alignment(0.00, 0.9999),
-                    end: Alignment(0.00, 0.22),
-                    colors: <Color>[
-                      Colors.white.withOpacity(0.9),
-                      Colors.transparent,
-                    ],
-                  ).createShader(bounds);
-                },
-                blendMode: BlendMode.dstOut,
-                child: ListView.builder(
-                  padding: const EdgeInsets.all(1),
-                  itemCount: tasks.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
+            ListView.builder(
+                shrinkWrap: true,
+                padding: const EdgeInsets.all(4),
+                itemCount: tasks.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return SingleChildScrollView(
+                    child: Container(
                       decoration: BoxDecoration(
                         color: checkboxes[index]
-                            ? Colors.green.withOpacity(0.7)
-                            : Colors.blue.withOpacity(0.7),
+                            ? Color.fromARGB(255, 112, 233, 116).withOpacity(0.7)
+                            : Color.fromARGB(255, 248, 70, 138).withOpacity(0.7),
                         borderRadius: BorderRadius.circular(8),
                       ),
-                      margin: EdgeInsets.all(2),
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.all(18.0),
-                              child: Row(
-                                children: [
-                                  Icon(
-                                    !checkboxes[index]
-                                        ? Icons.manage_history
-                                        : Icons.playlist_add_check_circle,
-                                    color: !checkboxes[index]
-                                        ? Colors.white
-                                        : Colors.white,
-                                    size: 32,
-                                  ),
-                                  SizedBox(width: 18),
-                                  Text(
-                                    '${tasks[index]}',
-                                    style: checkboxes[index]
-                                        ? TextStyle(
-                                            decoration:
-                                                TextDecoration.lineThrough,
-                                            fontSize: 20,
-                                            color:
-                                                Colors.black.withOpacity(0.5),
-                                          )
-                                        : TextStyle(fontSize: 20),
-                                  ),
-                                ],
-                              ),
-                            ),
+                          Icon(
+                            !checkboxes[index]
+                            ?Icons.manage_history 
+                            :Icons.playlist_add_check_circle,
+                            size: 32,
+                          ),
+                          SizedBox(width: 18),
+                          Text(
+                            '${tasks[index]}',
+                            style: checkboxes[index]
+                            ? TextStyle(
+                              decoration: TextDecoration.lineThrough,
+                              fontSize: 20,
+                              color: Colors.black.withOpacity(0.5)
+                            ) :TextStyle(fontSize: 20),
                           ),
                           Checkbox(
                             value: checkboxes[index],
@@ -155,53 +170,43 @@ class _MyHomePageState extends State<MyHomePage> {
                               setState(() {
                                 checkboxes[index] = newValue!;
                               });
-                              updateTaskCompletionStatus(
-                                  tasks[index], newValue!);
                             },
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.delete),
-                            onPressed: () {
-                              removeItem(index);
-                            },
-                          ),
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.delete),
+                              onPressed: null,
+                            )
                         ],
                       ),
-                    );
-                  },
-                ),
-              ),
-            ),
+                    ),
+                  );
+                }),
             Padding(
-              padding: EdgeInsets.only(top: 12, left: 25, right: 25),
+              padding: const EdgeInsets.only(top: 12, left: 25, right: 25),
               child: Row(
                 children: [
                   Expanded(
                     child: Container(
                       child: TextField(
                         controller: nameController,
-                        focusNode: _textFieldFocusNode,
+                        style: TextStyle(fontSize: 18),
                         maxLength: 20,
-                        style: TextStyle(fontSize: 18), // Set the text style
                         decoration: InputDecoration(
-                          contentPadding: EdgeInsets.all(
-                              16), // Add padding to the input area
+                          contentPadding: EdgeInsets.all(16),
                           border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(
-                                10), // Customize border radius
+                            borderRadius: BorderRadius.circular(10),
                           ),
-                          labelText: 'Add To-Do List Item',
+                          labelText: 'Wat you want??',
                           labelStyle: TextStyle(
-                              fontSize: 16,
-                              color: Colors.blue), // Customize label style
-                          hintText: 'Enter your task here', // Placeholder text
-                          hintStyle: TextStyle(
-                              fontSize: 16,
-                              color: Colors.grey), // Placeholder style
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 8, 14, 19),
+                          ),
+                          hintText: 'State your goal',
+                          hintStyle:
+                              TextStyle(fontSize: 16, color: Colors.grey),
                           focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Colors.blue,
-                                width: 2), // Custom focused border
+                            borderSide:
+                                BorderSide(color: Color.fromARGB(255, 63, 153, 226), width: 2),
                             borderRadius: BorderRadius.circular(10),
                           ),
                         ),
@@ -210,7 +215,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   IconButton(
                     icon: Icon(Icons.clear),
-                    onPressed: clearTextField,
+                    onPressed: null,
                   ),
                 ],
               ),
@@ -218,13 +223,12 @@ class _MyHomePageState extends State<MyHomePage> {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: ElevatedButton(
-                child: Text('Add To-Do Item'),
                 onPressed: () {
-                  _textFieldFocusNode.unfocus();
                   addItemToList();
                 },
+                child: Text('Add your goal'),
               ),
-            ),
+            )
           ],
         ),
       ),
